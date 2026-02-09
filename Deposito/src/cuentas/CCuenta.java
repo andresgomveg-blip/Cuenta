94829169fc3b5d63ba60c4701b55d6ed501d09ad
andresgomveg-blip <andres.gomveg@gmail.com>
2026-02-09 01:25:24 +0100

    2.0

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -1,18 +1,46 @@
 package cuentas;
 
+/**Clase que representa una cuenta bancaria.
+ * @author Andrés Gómez
+ */
+
 public class CCuenta {
 
+	/* Nombre del titular */
+	private String nombre;
 
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInterés;
+	/* Número de cuenta */
+	private String cuenta;
 
-    public CCuenta()
-    {
-    }
+	/* Saldo actual */
+	private double saldo;
 
-    public String getNombre() {
+	/* Tipo de interés */
+	private double tipoInterés;
+
+	/* Constructor por defecto */
+	public CCuenta() {
+	}
+
+	/** Constructor con parámetros
+	 * 
+	 * @param nombre Nombre del titular
+	 * 
+	 * @param cuenta Número de cuenta
+	 * 
+	 * @param saldo Saldo inicial
+	 * 
+	 * @param tipoInteres Tipo de interés
+	 */
+	public CCuenta(String nom, String cue, double sal, double tipo) {
+		nombre = nom;
+		cuenta = cue;
+		saldo = sal;
+	}
+
+	// Getters y setters
+
+	public String getNombre() {
 		return nombre;
 	}
 
@@ -44,31 +72,38 @@ public class CCuenta {
 		this.tipoInterés = tipoInterés;
 	}
 
-	public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
-    }
+	/**
+	 * Devuelve el saldo actual
+	 * 
+	 * @return saldo
+	 */
+	public double estado() {
+		return saldo;
+	}
 
-    public double estado()
-    {
-        return saldo;
-    }
+	/**
+	 * Ingresa una cantidad en la cuenta
+	 * 
+	 * @param cantidad Cantidad a ingresar
+	 * @throws Exception si la cantidad es negativa
+	 */
+	public void ingresar(double cantidad) throws Exception {
+		if (cantidad < 0)
+			throw new Exception("No se puede ingresar una cantidad negativa");
+		saldo = saldo + cantidad;
+	}
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
-    }
-
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
-    }
+	/**
+	 * Retira una cantidad de la cuenta
+	 * 
+	 * @param cantidad Cantidad a retirar
+	 * @throws Exception si la cantidad es incorrecta o no hay saldo
+	 */
+	public void retirar(double cantidad) throws Exception {
+		if (cantidad <= 0)
+			throw new Exception("No se puede retirar una cantidad negativa");
+		if (estado() < cantidad)
+			throw new Exception("No se hay suficiente saldo");
+		saldo = saldo - cantidad;
+	}
 }
